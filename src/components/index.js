bd87078c5fc12394f7214a5a7e923b721a4897ba
Nehal Jain <niihaljain2005@gmail.com>
2025-06-03 15:55:49 +0530

    add authentication components and forms for user login and signup

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
+import Select from "./Select.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import PostCard from "./PostCard.jsx"
+import PostForm from "./post-form/PostForm.jsx";
+import AuthLayout from "./AuthLayout.jsx"
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout
+};
